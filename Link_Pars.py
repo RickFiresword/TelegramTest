f30d1f25ceb16c8ed42924fb3e82390fce9b4ee3
RickFiresword <45540787+RickFiresword@users.noreply.github.com>
2020-03-22 22:13:50 +0200

    Update Link_Pars.py

--- Link_Pars.py
+++ Link_Pars.py
@@ -112,7 +112,7 @@ def executeSomething():
             
             #=======5H STATS========
             ff = open('2001-0.txt', 'r')
-            lines = ff.readlines().splitlines()
+            lines = ff.readlines()
             old_data = int(ff[1])
             
             h5_deaths = str((int(deaths_total) - int(data[1])))
