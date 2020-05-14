cec8a28f5836eea12d2dd6141ea49ade383a3051
RickFiresword <45540787+RickFiresword@users.noreply.github.com>
2020-05-14 19:24:17 +0300

    Update eSport.py

--- eSport.py
+++ eSport.py
@@ -172,8 +172,9 @@ def executeSomething():
 
                 # ===============Алгоритм 0 - первый тайм ======================#
 
-                if (int(the_time) >= 1 and int(the_time) <= 5) and (int(s_attacks_total) >= 15) and (int(d_attacks_total) >= 7) and (int(s_attacks_home) >= 6 and int(s_attacks_away) >= 6) and (int(d_attacks_home) >= 2 and int(d_attacks_away) >= 5) and int(score_total) == 1 and int(corner_total) <= 2:
-                        score_total_plus = str(score_total + 0.5)
+                #if (int(the_time) >= 1 and int(the_time) <= 5) and (int(s_attacks_total) >= 15) and (int(d_attacks_total) >= 7) and (int(s_attacks_home) >= 6 and int(s_attacks_away) >= 6) and (int(d_attacks_home) >= 2 and int(d_attacks_away) >= 5) and int(score_total) == 1 and int(corner_total) <= 2:
+                #        score_total_plus = str(score_total + 0.5)
+                if (int(the_time) >= 1 and int(the_time) <= 5)
                         first_time_over = ("‼️ TEST 🌊 eSport ‼️" + "\n" + "\n" + league_name + "\n" + teams_text + "\n" + "•" + "\n" + score_total_text + "\n" + the_time_text + "••••" + "\n" + '💵 ADVICE:\n' 'Bet: 2,5'+ " Over\n" + "➖➖➖➖➖➖➖➖➖➖\ ")
                         # чтение файла
                         handle = open("text.text", "r")
