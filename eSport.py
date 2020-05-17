ea5025852c0d8d375e15b14e6610f058f1f0d978
Rick_Firesword <aov.vasiliev@gmail.com>
2020-05-17 19:31:08 +0300

    d
    d

--- eSport.py
+++ eSport.py
@@ -186,7 +186,7 @@ def executeSomething():
                             str_id_get = str(id_get)
                             print (time.strftime("%H:%M:%S  ") + 'last message ID: ' + str_id_get)
                             #first_time_over2 = ("❗ Wave_" + new_id_get + "   TEST eSport ❗"+ "\n" + "\n" + league_name + "\n" + teams_text + "\n" + "•" + "\n" + score_total_text + "\n" + the_time_text + "\n" + "••••" + "\n" + '💵 ADVICE:\n' 'Bet: 2,5' + " Over " +'\n' + "➖➖➖➖➖➖➖➖➖➖\ ")
-                            first_time_over2 = (new_id_get)
+                            first_time_over2 = (id_get, "\n", get_sport_id)
 
                             #bot.editMessageText(chat_id=my_telegram_chat_id, message_id=id_get, text=first_time_over2)
                             bot.editMessageText(telepot.message_identifier(this_message), text=first_time_over2)
