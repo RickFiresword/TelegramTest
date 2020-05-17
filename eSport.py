a8da673debf423a72e943bcbf7a0bd6e8c49709a
Rick_Firesword <aov.vasiliev@gmail.com>
2020-05-17 19:52:47 +0300

    f
    f

--- eSport.py
+++ eSport.py
@@ -172,7 +172,7 @@ def executeSomething():
                             pass
                         else:
                             # ЗАПИСЬ в файл начало
-                            f = open('text2.text', 'a')
+                            f = open('text.text', 'a')
                             a = str(get_sport_id,"\n")
                             f.write(a)
                             f.close()
