4b56027e010dea77f9b9041ccfa7192d55839031
Rick_Firesword <aov.vasiliev@gmail.com>
2020-05-18 17:16:47 +0300

    s
    s

--- eSport.py
+++ eSport.py
@@ -162,7 +162,7 @@ def executeSomething():
 
                 # =============== КИБЕР ФУТБОЛ ======================#
 
-                if (int(the_time) >= 1 and int(the_time) <= 5) and (int(s_attacks_total) >= 15) and (int(d_attacks_total) >= 7) and (int(s_attacks_home) >= 6 and int(s_attacks_away) >= 6) and (int(d_attacks_home) >= 2 and int(d_attacks_away) >= 5) and int(score_total) == 1 and int(corner_total) <= 2:
+                if (int(the_time) >= 1 and int(the_time) <= 5) and (int(s_attacks_total) >= 15) and (int(d_attacks_total) >= 7) and (int(s_attacks_home) >= 6 and int(s_attacks_away) >= 6) and (int(d_attacks_home) >= 2 and int(d_attacks_away) >= 5) and int(score_total) == 1 and int(corner_total) <= 2 and int(red_total) == 0:
                 #if the_time > 0:
                         score_total_plus = str(score_total + 0.5)
                         first_time_over = ("‼️ NEW 🌊 WAVE ‼️" + "\n" + "\n" + league_name + "\n" + teams_text + "\n" + "•" + "\n" + score_total_text + "\n" + the_time_text + "\n••••" + "\n" + '💵 ADVICE:\n' 'Bet: 2,5'+ " Over\n" + "➖➖➖➖➖➖➖➖➖➖\ ")
