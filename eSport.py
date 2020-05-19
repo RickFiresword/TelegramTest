56a8e4cecf798a186e1f8d079efaadc5ac93fe8e
Rick_Firesword <aov.vasiliev@gmail.com>
2020-05-19 12:58:50 +0300

    s
    s

--- eSport.py
+++ eSport.py
@@ -47,6 +47,7 @@ def executeSomething():
         
 
         get_sport_id = j['id']
+        get_sport_id = int(get_sport_id)
         event_view = "https://api.betsapi.com/v1/event/view?token=27503-R9VUM6NP7900Cw&event_id=" + get_sport_id
 
         r2 = requests.get(event_view).json()
