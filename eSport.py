33e36d1a8e72d9d2e2e5ac888cf6a2dfc0552ea8
Rick_Firesword <aov.vasiliev@gmail.com>
2020-05-19 12:50:01 +0300

    f
    f

--- eSport.py
+++ eSport.py
@@ -43,16 +43,16 @@ def executeSomething():
         r = 0
         print (time.strftime("%H:%M:%S  ") + "Errore -r-!")
 
-    for j in r:
+    try:
+        for j in r:
         #new
-        try:
-            get_sport_id = j['id']
-            event_view = "https://api.betsapi.com/v1/event/view?token=27503-R9VUM6NP7900Cw&event_id=" + get_sport_id
+                get_sport_id = j['id']
+                event_view = "https://api.betsapi.com/v1/event/view?token=27503-R9VUM6NP7900Cw&event_id=" + get_sport_id
 
-            r2 = requests.get(event_view).json()
-            time.sleep(0)
-        except (IndexError, KeyError, ValueError, TypeError):
-            pass
+                r2 = requests.get(event_view).json()
+                time.sleep(0)
+    except (IndexError, KeyError, ValueError, TypeError):
+        pass
         #pass
 
 
