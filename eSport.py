4414418834e05f063146320caa6c9cc223ffff9b
Rick_Firesword <aov.vasiliev@gmail.com>
2020-05-24 23:19:27 +0300

    Update eSport.py

--- eSport.py
+++ eSport.py
@@ -34,54 +34,24 @@ rr = 0
 def executeSomething():
 
     try:
-        r = requests.get(url).json()
-        r = r['results']
-        r = r[::-1]
 
+        for a in range(10000):
+            #--------------<Settings>----------------->
+            
 
-    except (IndexError, KeyError, ValueError):
-        time.sleep(5)
-        executeSomething()
-        print ("Errore with R ======")
+            handle = open("all_links.txt", "r")
+            data = handle.read().splitlines()
 
-    for j in r:
-        get_sport_id = j['id']
-        event_view = "https://api.betsapi.com/v1/event/view?token=27503-R9VUM6NP7900Cw&event_id=" + str(get_sport_id)
+            for i in data:
+                rr = 'https://api.betsapi.com/v1/event/view?token=27503-R9VUM6NP7900Cw&event_id=' + i
+                rr = rr.replace('.text','')
+                r = requests.get(rr).text
+                soup = BeautifulSoup(r, features="html.parser")
+                handle2 = open(i +'.txt', "r")
+                data2 = handle2.read().splitlines()
+                data2 = data2[0]
 
-        r2 = requests.get(event_view).json()
-        #r3 = requests.get(odds_view).json()
-        time.sleep(0)
-
-        try:
-            r2 = r2['results']
-
-
-        except (IndexError, KeyError, ValueError, TypeError):
-            pass
-            print (time.strftime("%H:%M:%S  ") + "Errore -r2-! ID: " + get_sport_id)
-
-
-
-
-        try:
-
-            for a in range(10000):
-                #--------------<Settings>----------------->
-                
-
-                handle = open("all_links.txt", "r")
-                data = handle.read().splitlines()
-
-                for i in data:
-                    rr = 'https://api.betsapi.com/v1/event/view?token=27503-R9VUM6NP7900Cw&event_id=' + i
-                    rr = rr.replace('.text','')
-                    r = requests.get(rr).text
-                    soup = BeautifulSoup(r, features="html.parser")
-                    handle2 = open(i +'.txt', "r")
-                    data2 = handle2.read().splitlines()
-                    data2 = data2[0]
-
-                    print (data2)
+                print (data2)       
                     
 
 
