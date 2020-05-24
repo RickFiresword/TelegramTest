c9bf6aba61d0a12d9a2bef62840976658c73077b
Rick_Firesword <aov.vasiliev@gmail.com>
2020-05-25 00:22:49 +0300

    Update eSport.py

--- eSport.py
+++ eSport.py
@@ -49,43 +49,46 @@ def executeSomething():
                 r = requests.get(rr).json()
                 r = r['results']
                 r = r[::-1]
+
                 for a in r:
-                    time_status = a['time_status']
-                    time_status = int(time_status)
-                    #-Names
-                    league = a['league']['name']
-                    team_home = a['home']['name']
-                    team_away = a['away']['name']
-                    the_time = 2
-                    score_home_str = int(a['scores']['2']['home'])
-                    score_away_str = int(a['scores']['2']['away'])
-                    score_home = int(score_home_str)
-                    score_away = int(score_away_str)
-                    score_total = score_away+score_home
-
-
-
-
-                
-                    handle2 = open(i +'.txt', "r")
-                    data2 = handle2.read().splitlines()
-                    data2 = data2[0]
-                    data2 = int(data2)
-                    new_id_get = data2+9
-                    new_id_get = str(new_id_get)
-
-                    first_time_over1 = ("❗ Wave_" + new_id_get + "  eSport ❗"+ "\n" + "\n" + league_name + "\n" + teams_text + "\n" + "•" + "\n" + score_total_text + "\n" + the_time_text + "\n" + "••••" + "\n" + '💵 ADVICE:\n' 'Bet: 15% on: ' + score_total_plus + " Over HT \nBet: 85% on 1.5 Over FT" +'\n' + "➖➖➖➖➖➖➖➖➖➖\ ")
-                    first_time_over2 = ("❗ Wave_" + new_id_get + "  eSport ❗"+ "\n" + "\n" + league_name + "\n" + teams_text + "\n" + "•" + "\n" + score_total_text + "\n" + the_time_text + "\n" + "••••" + "\n" + '💵 ADVICE:\n' 'Bet: 15% on: ' + score_total_plus + " Over HT \nBet: 85% on 1.5 Over FT" +'\n' + "➖➖➖➖➖➖➖➖➖➖\ \n XXX")
-
                     try:
-                        if time_status > 0:
-                            print ("STATUS > 2")
-                            #bot.editMessageText(chat_id=my_telegram_chat_id, message_id=data2, text="тру-ту-ту")
-                            bot.editMessageText((my_telegram_chat_id, data2), first_time_over2)
-                            #bot.editMessageText(telepot.message_identifier(data2), text="SUCKKKKKKKK")
+                        time_status = a['time_status']
+                        time_status = int(time_status)
+                        #-Names
+                        league = a['league']['name']
+                        team_home = a['home']['name']
+                        team_away = a['away']['name']
+                        the_time = 2
+                        score_home_str = int(a['scores']['2']['home'])
+                        score_away_str = int(a['scores']['2']['away'])
+                        score_home = int(score_home_str)
+                        score_away = int(score_away_str)
+                        score_total = score_away+score_home
+
+
+
+
+                    
+                        handle2 = open(i +'.txt', "r")
+                        data2 = handle2.read().splitlines()
+                        data2 = data2[0]
+                        data2 = int(data2)
+                        new_id_get = data2+9
+                        new_id_get = str(new_id_get)
+
+                        first_time_over1 = ("❗ Wave_" + new_id_get + "  eSport ❗"+ "\n" + "\n" + league_name + "\n" + teams_text + "\n" + "•" + "\n" + score_total_text + "\n" + the_time_text + "\n" + "••••" + "\n" + '💵 ADVICE:\n' 'Bet: 15% on: ' + score_total_plus + " Over HT \nBet: 85% on 1.5 Over FT" +'\n' + "➖➖➖➖➖➖➖➖➖➖\ ")
+                        first_time_over2 = ("❗ Wave_" + new_id_get + "  eSport ❗"+ "\n" + "\n" + league_name + "\n" + teams_text + "\n" + "•" + "\n" + score_total_text + "\n" + the_time_text + "\n" + "••••" + "\n" + '💵 ADVICE:\n' 'Bet: 15% on: ' + score_total_plus + " Over HT \nBet: 85% on 1.5 Over FT" +'\n' + "➖➖➖➖➖➖➖➖➖➖\ \n XXX")
+
+                        try:
+                            if time_status > 0:
+                                print ("STATUS > 2")
+                                #bot.editMessageText(chat_id=my_telegram_chat_id, message_id=data2, text="тру-ту-ту")
+                                bot.editMessageText((my_telegram_chat_id, data2), first_time_over2)
+                                #bot.editMessageText(telepot.message_identifier(data2), text="SUCKKKKKKKK")
+                        except:
+                            pass
                     except:
                         pass
-
 
 
 
