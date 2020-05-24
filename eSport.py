1cb177bcd7eecc8b149beca0b6ea596234e225cb
Rick_Firesword <aov.vasiliev@gmail.com>
2020-05-24 23:55:31 +0300

    Update eSport.py

--- eSport.py
+++ eSport.py
@@ -61,7 +61,7 @@ def executeSomething():
                     if time_status > 0:
                         print ("STATUS > 2")
                         #bot.editMessageText(chat_id=my_telegram_chat_id, message_id=data2, text="SUCKKKKKKKK")
-                        bot.editMessageText(telepot.message_identifier(data2), text="SUCKKKKKKKK")
+                        bot.editMessageText(data2, text="SUCKKKKKKKK")
 
 
 
