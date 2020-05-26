991aadc7b7fbd2f9ce58411e4e92589d43ecc836
Rick_Firesword <aov.vasiliev@gmail.com>
2020-05-26 11:39:16 +0300

    Update eSport.py

--- eSport.py
+++ eSport.py
@@ -111,7 +111,7 @@ def executeSomething():
                 try:
                     time.sleep(0)
                     
-                    if str(league) == "Esoccer Live Arena - 10 mins play" and (score_total <= 2) and int(time_status) == 3:
+                    if str(league) == "Esoccer Live Arena - 10 mins play" and (score_total <= '2') and int(time_status) == 3:
                         print(team_home + team_away)
                         bot.editMessageText((my_telegram_chat_id, data2), first_time_over4)
 
